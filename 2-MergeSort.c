11aa87ad8ea6878c8ed01ff5ecbf6eb616f6d41c
Tanishq Raj <tanishqraj306@gmail.com>
2024-05-17 12:55:57 +0530

    Fixed Merge Sort

--- 2-MergeSort.c
+++ 2-MergeSort.c
@@ -10,7 +10,7 @@ void merge(int arr[], int l, int m, int r) {
   int L[n1], R[n2];
 
   for (i = 0; i < n1; i++) {
-    L[i] = arr[l + 1];
+    L[i] = arr[l + i];
   }
   for (j = 0; j < n2; j++) {
     R[j] = arr[m + 1 + j];
@@ -42,7 +42,7 @@ void merge(int arr[], int l, int m, int r) {
 
 void mergeSort(int arr[], int l, int r) {
   if (l < r) {
-    int m = l + (r - 1) / 2;
+    int m = l + (r - l) / 2;
     mergeSort(arr, l, m);
     mergeSort(arr, m + 1, r);
     merge(arr, l, m, r);
@@ -61,7 +61,7 @@ int main() {
   fprintf(fp, "n,Time(ms)\n");
 
   for (n = 5000; n <= 10000; n += 1000) {
-    int *arr = (int *)malloc(sizeof(int));
+    int *arr = (int *)malloc(n * sizeof(int));
     srand(time(NULL));
     for (i = 0; i < n; i++) {
       arr[i] = rand();
@@ -70,7 +70,7 @@ int main() {
     mergeSort(arr, 0, n - 1);
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
-    printf("Time taken to sort %d elements: %f", n, cpu_time_used);
+    printf("Time taken to sort %d elements: %f\n", n, cpu_time_used);
     fprintf(fp, "%d,%f", n, cpu_time_used);
     free(arr);
   }
